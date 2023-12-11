61c5396630aed84613df48c20287d350e1a978b6
Gabriel Broslavschi <gabrielbroslavschidacosta@gmail.com>
2023-12-11 16:04:53 -0300

    feat: Criar Projeto Usando Vue CLI

---
+++ componentes/component-intro/src/App.vue
@@ -0,0 +1,20 @@
+<!-- eslint-disable vue/valid-template-root -->
+
+<template>
+  <div>
+    <h1>Contadores</h1>
+    {{ contador }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+};
+</script>
+
+<style></style>
