1ccec33b4fcf30f4193c37fa287e2f76b9b64f1f
Gabriel Broslavschi <gabrielbroslavschidacosta@gmail.com>
2023-12-12 16:25:18 -0300

    feat: Comunicação entre Componentes Irmãos

--- componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -3,6 +3,7 @@
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
         <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
+        <p>Idade do usuário: <strong>{{ idade }}</strong></p>
         <button @click='reiniciarNome'>Reiniciar Nome</button>
         <button @click="reiniciarFn"> Reiniciar NOme ( CallBack ) </button>
     </div>
@@ -18,7 +19,9 @@ export default {
         // default: function () {
         //     return Array(10).fill(0).join(',')
         // }
-        
+        },
+        idade:{
+            type: Number
         },
         reiniciarFn: Function
     },
