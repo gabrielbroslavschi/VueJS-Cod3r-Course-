6051553b8ebe2487ce377fe9207c7de9e4194492
Gabriel Broslavschi <gabrielbroslavschidacosta@gmail.com>
2023-12-12 16:13:36 -0300

    feat: Comunicação Indireta com Callback

--- componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -4,6 +4,7 @@
         <p>Vários detalhes...</p>
         <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
         <button @click='reiniciarNome'>Reiniciar Nome</button>
+        <button @click="reiniciarFn"> Reiniciar NOme ( CallBack ) </button>
     </div>
 </template>
 
@@ -13,11 +14,13 @@ export default {
         nome: {
         type: String,
         // required: true,
-        default: "Anonimo"
+        default: "Anonimo",
         // default: function () {
         //     return Array(10).fill(0).join(',')
         // }
-        }
+        
+        },
+        reiniciarFn: Function
     },
     methods:{
         inverterNome(){
