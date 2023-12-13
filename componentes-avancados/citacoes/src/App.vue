aae78127093a8f32ee4c0a6f1a73156735d8746f
Gabriel Broslavschi <gabrielbroslavschidacosta@gmail.com>
2023-12-13 16:57:11 -0300

    feat: Mantendo o Componente Dinâmico Vivo

--- componentes-avancados/citacoes/src/App.vue
+++ componentes-avancados/citacoes/src/App.vue
@@ -4,7 +4,10 @@
 			<button @click='componente = "Citacoes"'>Citações</button>
 			<button @click='componente = "Sobre"'>Sobre</button>
 		</span>
-		<component :is="componente"/>
+		<keep-alive>
+			<component :is="componente"/>
+		</keep-alive>
+		
 	</div>
 </template>
 
