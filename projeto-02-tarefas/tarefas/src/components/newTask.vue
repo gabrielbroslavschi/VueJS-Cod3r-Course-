fc1d54ab72905e7d21597c0900894962eb84ed81
Gabriel Broslavschi <gabrielbroslavschidacosta@gmail.com>
2023-12-14 16:22:59 -0300

    feat: Componente Nova Tarefa #02

--- projeto-02-tarefas/tarefas/src/components/newTask.vue
+++ projeto-02-tarefas/tarefas/src/components/newTask.vue
@@ -1,8 +1,8 @@
 <!-- eslint-disable vue/valid-template-root -->
 <template>
     <div class='new-task'>
-        <input v-model="name" @keydown.enter="add" type="text">
-        <button @click='add'>+</button>
+        <input v-model="name" @keydown.enter="add" type="text" class='form-element' placeholder="Nova Tarefa">
+        <button class='form-element' @click='add'>+</button>
     </div>
 </template>
 
@@ -23,4 +23,32 @@ export default {
 </script>
 
 <style>
+
+.new-task{
+    margin: 35px;
+}
+
+.form-element{
+    outline: none;
+    font-size: 2rem;
+    border: 1px solid #fff;
+    padding: 5px 10px 8px;
+    color: #fff;
+}
+
+input.form-element{
+    width: 500px;
+    background: #fff2;
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+    border-right: none;
+}
+
+button.form-element{
+    background-color: #2196f3;
+    border-left: none;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+}
+
 </style>
