ef7b3bacef1f1ef4142d6f1ec5c6702d3ed9ea15
Gabriel Broslavschi <gabrielbroslavschidacosta@gmail.com>
2023-12-20 16:03:59 -0300

    feat: Duplicando Código para Usar os Mixins

--- filtros-mixins/filtros-mixins-exercicios/src/App.vue
+++ filtros-mixins/filtros-mixins-exercicios/src/App.vue
@@ -7,40 +7,65 @@
 		<p>{{ cpf | cpfFilter | inverter }}</p>
 
 		<input type="text" :value='cpf | cpfFilter | inverter' >
+
+		<hr>
+
+		<Frutas></Frutas>
+
+		<hr>
+
+		<div>
+        <ul>
+            <li v-for='fruta in frutas' :key='fruta'>{{ fruta }}</li>
+        </ul>
+
+        <input type="text" v-model='fruta' @keydown.enter="add">
+    </div>
 	</div>
 </template>
 
 <script>
+import Frutas from "./Frutas.vue";
+
 export default {
-	filters:{
-		cpfFilter(valor){
-			const arr = valor.split('');
-			arr.splice(3, 0, '.');
-			arr.splice(7, 0, '.');
-			arr.splice(11, 0, '-');
-			return arr.join('');
-		}
-	},
-	data(){
-		return{
-			cpf: "12345678910"
-		}
-	}
-}
+  components: { Frutas },
+  filters: {
+    cpfFilter(valor) {
+      const arr = valor.split("");
+      arr.splice(3, 0, ".");
+      arr.splice(7, 0, ".");
+      arr.splice(11, 0, "-");
+      return arr.join("");
+    },
+  },
+  data() {
+    return {
+      cpf: "12345678910",
+      fruta: "",
+      frutas: ["banana", "maça", "laranja"],
+    };
+  },
+  methods: {
+    add() {
+      this.frutas.push(this.fruta);
+      this.fruta = "";
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 2.5rem;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.5rem;
 }
 
-input{
-	font-size: 2.5rem;
+input {
+  font-size: 2.5rem;
 }
 </style>
