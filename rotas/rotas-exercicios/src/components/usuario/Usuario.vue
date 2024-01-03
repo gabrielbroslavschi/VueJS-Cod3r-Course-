866acae268a1727891a2a83abc115869059a861f
Gabriel Broslavschi <gabrielbroslavschidacosta@gmail.com>
2024-01-03 16:36:24 -0300

    feat: Lendo e Usando Parâmetros de Rotas

--- rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -2,12 +2,18 @@
     <div class="usuario">
         <h2>Usuários</h2>
         <hr>
+        <p><strong>Codigo: </strong>{{ id }}</p>
         <button sucesso @click="irParaInicio">Voltar</button>
     </div>
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            id: this.$route.params.id
+        }
+    },
     methods:{
         irParaInicio(){
             this.$router.push("/")
