96da94629a5fccaf0081fc257a9badca637a9481
Gabriel Broslavschi <gabrielbroslavschidacosta@gmail.com>
2024-01-04 17:13:18 -0300

    feat: Tornando Router Links mais Dinâmico

--- rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -3,26 +3,27 @@
         <h3>Usuário Detalhe</h3>
         <hr>
         <p><strong>Codigo: </strong>{{ id }}</p>
-
+        <router-link tag='button' primario :to="`/usuario/${id}/editar`">
+            Editar
+        </router-link>
     </div>
 </template>
 
 <script>
-export default{
-    props: ["id"]
-    // data(){
-    //     return{
-    //         id: this.$route.params.id
-    //     }
-    // },
-    // watch:{
-    //     $route(to){
-    //         this.id = to.params.id;
-    //     },
-    // },
-}
+export default {
+  props: ["id"],
+  // data(){
+  //     return{
+  //         id: this.$route.params.id
+  //     }
+  // },
+  // watch:{
+  //     $route(to){
+  //         this.id = to.params.id;
+  //     },
+  // },
+};
 </script>
 
 <style>
-
 </style>
