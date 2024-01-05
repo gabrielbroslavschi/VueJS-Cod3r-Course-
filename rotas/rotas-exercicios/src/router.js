cb1256b67167332311830d1b4b58899391698f70
Gabriel Broslavschi <gabrielbroslavschidacosta@gmail.com>
2024-01-05 12:49:40 -0300

    feat: Configurando Rota 'Pega Tudo'

--- rotas/rotas-exercicios/src/router.js
+++ rotas/rotas-exercicios/src/router.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Inicio from "./components/Inicio.vue";
 import Usuario from "./components/usuario/Usuario.vue";
-import Menu from './components/template/Menu.vue';
+import Menu from "./components/template/Menu.vue";
 import UsuarioLista from "./components/usuario/UsuarioLista.vue";
 import UsuarioDetalhe from "./components/usuario/UsuarioDetalhe.vue";
 import UsuarioEditar from "./components/usuario/UsuarioEditar.vue";
@@ -11,23 +11,37 @@ Vue.use(Router);
 
 export default new Router({
   routes: [
-    { 
+    {
       name: "inicio",
-      path: "/", 
-    // component: Inicio ,
-    components:{
-      default: Inicio,
-      menu: Menu
-    }
-  },
-    { path: "/usuario", component: Usuario, props: true, children: [
-        {path: "", component: UsuarioLista, props: true},
-        {path: ":id", component: UsuarioDetalhe, props: true},
-        {path: ":id/editar", component: UsuarioEditar, props: true, name: 'editarUsuario'}
-    ] },
-  {
-    path: "/redirecionar",
-    redirect: "/usuario"
-  }
+      path: "/",
+      // component: Inicio ,
+      components: {
+        default: Inicio,
+        menu: Menu,
+      },
+    },
+    {
+      path: "/usuario",
+      component: Usuario,
+      props: true,
+      children: [
+        { path: "", component: UsuarioLista, props: true },
+        { path: ":id", component: UsuarioDetalhe, props: true },
+        {
+          path: ":id/editar",
+          component: UsuarioEditar,
+          props: true,
+          name: "editarUsuario",
+        },
+      ],
+    },
+    {
+      path: "/redirecionar",
+      redirect: "/usuario",
+    },
+    {
+      path: "*",
+      redirect: "/",
+    },
   ],
 });
