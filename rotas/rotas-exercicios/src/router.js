6d44e1dd1d8a055834b2354be37c12f1c063b95a
Gabriel Broslavschi <gabrielbroslavschidacosta@gmail.com>
2024-01-05 13:25:52 -0300

    feat: Carregando Rotas Tardiamente

--- rotas/rotas-exercicios/src/router.js
+++ rotas/rotas-exercicios/src/router.js
@@ -1,13 +1,19 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Inicio from "./components/Inicio.vue";
-import Usuario from "./components/usuario/Usuario.vue";
 import Menu from "./components/template/Menu.vue";
-import UsuarioLista from "./components/usuario/UsuarioLista.vue";
-import UsuarioDetalhe from "./components/usuario/UsuarioDetalhe.vue";
-import UsuarioEditar from "./components/usuario/UsuarioEditar.vue";
+// import Usuario from "./components/usuario/Usuario.vue";
+// import UsuarioLista from "./components/usuario/UsuarioLista.vue";
+// import UsuarioDetalhe from "./components/usuario/UsuarioDetalhe.vue";
+// import UsuarioEditar from "./components/usuario/UsuarioEditar.vue";
 
 Vue.use(Router);
+
+const Usuario = () => import(/* webpackChunkName: 'usuario'*/'./components/usuario/Usuario.vue');
+const UsuarioEditar = () => import(/* webpackChunkName: 'usuarioEditar'*/ './components/usuario/UsuarioEditar.vue');
+const UsuarioLista = () => import(/* webpackChunkName: 'usuarioLista'*/ './components/usuario/UsuarioLista.vue');
+const UsuarioDetalhe = () => import(/* webpackChunkName: 'usuarioDetalhe'*/ './components/usuario/UsuarioDetalhe.vue');
+
 
 const router = new Router({
   mode: "history",
@@ -65,7 +71,7 @@ router.beforeEach((to, from, next) =>{
 
   next();
 
-  
+
   // if(to.path !== '/usuario'){
   //   next("/usuario");
   // }else{
