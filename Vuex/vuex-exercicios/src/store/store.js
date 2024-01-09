e871bd412e3feccab280b739e5b50b670328bc2a
Gabriel Broslavschi <gabrielbroslavschidacosta@gmail.com>
2024-01-09 16:24:40 -0300

    feat: Modularizando o Gerenciador de Estado

--- Vuex/vuex-exercicios/src/store/store.js
+++ Vuex/vuex-exercicios/src/store/store.js
@@ -1,39 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import carrinho from "./modulos/carrinho";
+import parametros from "./modulos/parametros";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    produtos: [],
-    quantidade: 2,
-    preco: 9.99,
-  },
-  getters: {
-    valorTotal(state) {
-      return state.produtos
-        .map((p) => p.quantidade * p.preco)
-        .reduce((total, atual) => total + atual, 0);
-    },
-  },
-  mutations:{
-    //payLoad === produto
-    adicionarProduto(state, payLoad){
-        state.produtos.push(payLoad);
-    },
-    setQuantidade(state, payload){
-        state.quantidade = payload;
-    },
-    setPreco(state, payload){
-        state.preco = payload;
-    }
-
-  },
-  actions:{
-    adicionarProduto({commit}, payload){
-        setTimeout(() => {
-            commit("adicionarProduto", payload)
-        }, 1000)
-    }
+  modules:{
+    carrinho,
+    parametros
   }
 });
